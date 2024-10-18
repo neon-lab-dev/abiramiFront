324c35aa6a891e1f1052722805808b2f78f1b117
PrernaBadwane <95902077+PrernaBadwane@users.noreply.github.com>
2024-10-18 22:17:13 +0530

    reusable table

---
+++ src/pages/Clients/ListPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ClientTable from '../../Components/Clients/ClientTable'
+
+const ListPage = () => {
+  return (
+  < ClientTable/>
+  )
+}
+
+export default ListPage
---
+++ src/pages/Suppliers/ListPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import SuppliersTable from '../../Components/Suppliers/SuppliersTable'
+
+const ListPage = () => {
+  return (
+    <SuppliersTable/>
+  )
+}
+
+export default ListPage
