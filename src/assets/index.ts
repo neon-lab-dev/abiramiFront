cb9a7fc4187b253da3ae909cc19064df90f5789c
JobaerAbdi <jobaerrahman757@gmail.com>
2024-10-20 16:01:31 +0600

    button input and statusCard done

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,5 +1,15 @@
 // Import icons here
-import bell from "../assets/icons/Bell.svg"
+import bell from "../assets/icons/Bell.svg";
+import invoices from "../assets/icons/invoices.svg";
+import paidInvoices from "../assets/icons/paidInvoices.svg";
+import pendingInvoice from "../assets/icons/pendingInvoice.svg";
+import incomeReceived from "../assets/icons/incomeReceived.svg";
+import invoiceMonth from "../assets/icons/invoiceMonth.svg";
+import salesMonth from "../assets/icons/salesMonth.svg";
+import purchaseMonth from "../assets/icons/purchaseMonth.svg";
+import totalPurchase from "../assets/icons/totalPurchase.svg";
+import InputField from "../assets/icons/inputField.svg";
+import button from "../assets/icons/button.svg";
 
 
 
@@ -10,6 +20,16 @@ import bell from "../assets/icons/Bell.svg"
 
 export const ICONS = {
     bell,
+    invoices,
+    paidInvoices,
+    pendingInvoice,
+    incomeReceived,
+    invoiceMonth,
+    salesMonth,
+    purchaseMonth,
+    totalPurchase,
+    InputField,
+    button
 }
 
 
