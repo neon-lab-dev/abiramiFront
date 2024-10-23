c22035ae23ad5ea892449fe89d9df63e0cf81561
PrernaBadwane <95902077+PrernaBadwane@users.noreply.github.com>
2024-10-24 00:07:32 +0530

    invoice table

--- src/Components/Shared/Table/Table.tsx
+++ src/Components/Shared/Table/Table.tsx
@@ -186,7 +186,7 @@ const Table: React.FC<TableProps> = ({
                     {row.iconsOrder.map((icon:string) => {
                       if (icon === "i1" && row.i1) {
                         return (
-                          <div className={`${i1CustomClass(row.i1, row.i2, row.i3)} max-w-[46px]`}>
+                          <div className={`${i1CustomClass(row.i1, row.i2, row.i3)} max-w-[46px] mr-${(row.i1 && row.i2)&& !row.i3 ? "2" : "0"}`}>
                           <button
                             key="i1"
                             onClick={() => handleEdit(row)}
@@ -206,23 +206,18 @@ const Table: React.FC<TableProps> = ({
                      
                       if (icon === "i2" && row.i2) {
                         return (
-                          <div className="flex items-center gap-2 ml-2">
-                          <img src={ICONS.graybar} alt="|" className="h-3 w-[2px]"/>
-                          <button
-                            key="i2"
-                            onClick={() => handleApprove(row)}
-                            className={`rounded-full h-6 w-6 flex items-center justify-center ${bg_i2}   `}
-                          >
-                            <img
-                              src={icons.i2}
-                              alt="i2"
-                              className="h-3 w-3"
-                            />
-                          </button>
+                          <div className={`flex items-center gap-${(row.i1 && row.i2 )&&! row.i3 ? "4" : "2"} ml-${(row.i1 && row.i2 )&&! row.i3  ? "4" : "2"}`}>
+                            <img src={ICONS.graybar} alt="|" className="h-3 w-[2px]" />
+                            <button
+                              key="i2"
+                              onClick={() => handleApprove(row)}
+                              className={`rounded-full h-6 w-6 flex items-center justify-center ${bg_i2}`}
+                            >
+                              <img src={icons.i2} alt="i2" className="h-3 w-3" />
+                            </button>
                           </div>
                         );
                       }
-                      
                       if (icon === "i3" && row.i3) {
                         return (
                           <div className={`flex items-center gap-2 ml-0 ${i3CustomClass(row.i1, row.i2, row.i3)}`}>
